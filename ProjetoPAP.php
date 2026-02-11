2776a3b90d3176220d1ddab667c78cc31acadf11
salvador07-jpg <salvacruz200708@gmail.com>
2026-02-11 16:42:54 +0000

    teste commit

---
+++ ProjetoPAP.php
@@ -0,0 +1,27 @@
+<html lang = "pt">
+	<head>
+		<meta charset = "UTF-8">
+			<title>Trabalho Site</title>
+			<link rel = "stylesheet" type = "text/css" href = "FolhaEstiloProjetoPAP.css">
+	</head>
+    <main>
+	    <header>
+	        <h1>Projeto PAP</h1>
+		</header>
+
+		<nav class = "menu">
+			<ul>
+				<p><a href = "####">opcao1</a></p>
+				<p><a href = "####">opcao2</a></p>
+			</ul>
+		</nav>
+
+		<article>
+			<p> bjabababababab</p>
+		</article>
+
+		<footer>
+			<p>Projeto PAP</p>
+		</footer>
+	<main>
+<html>
