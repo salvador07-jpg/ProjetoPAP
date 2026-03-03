f56905999cb6d8f82204c3fc9035d265fd4f600a
salvador07-jpg <salvacruz200708@gmail.com>
2026-03-03 23:06:13 +0000

    Primeiro commit projeto PAP

--- ProjetoPAP.php
+++ ProjetoPAP.php
@@ -1,31 +1,152 @@
+<!DOCTYPE html>
 <html lang = "pt">
 	<head>
 		<meta charset = "UTF-8">
 			<title>Trabalho Site</title>
-			<link rel = "stylesheet" type = "text/css" href = "FolhaEstiloProjetoPAP.css">
+			<link rel = "stylesheet" href = "FolhaEstiloProjetoPAP.css">
+			<link rel = "stylesheet" href = "FeMenuLateral.css">
+			<link rel = "stylesheet" href = "FeSearch.css">
+
+			<!-- Botões -->
+			<link rel = "stylesheet" href = "FeBtAparenciaBotoes.css">
+			<link rel = "stylesheet" href = "FeBtMenu.css">
+			<link rel = "stylesheet" href = "FeBtUtilizador.css">
+			<link rel = "stylesheet" href = "FeBtTemaSite.css">
+			<link rel = "stylesheet" href = "FeBtAddBD.css">
+
+			<!-- Ficheiro responsivo é por último para conseguir se sobrepor aos estilos base -->
+			<link rel = "stylesheet" href = "FeLayoutResponsivo.css">
 	</head>
     <main>
 	    <header>
-	        <h1>Projeto PAP</h1>
+			<!-- Botão menu lateral -->
+			<div class = "LeftMenu-Skin">
+				<button id = "LeftMenu-ButtonJS" class = "BtLeftMenu"> 
+					<img style = "width: 20px; height: 20px;" src = "imagens/menu.png">
+				</button>
+				<div id = "BtLeftMShow" class = "leftMenu-Items">	
+					<div class = "CloseMenu">
+						<button id = "BtLeftMenu-CloseJS" class = "BtCloseMenu"> 
+							<img style = "width: 20px; height: 20px;" src = "imagens/close.png">
+						</button>
+					</div>
+					<a href = "####">opcao1</a>
+					<a href = "####">opcao2</a>
+					<div class = "UserMenu-Options">
+						<button id = "BtLogInJS" class = "BtLogIn">LogIn</button>
+						<button id = "BtSignInJS" class = "BtSignIn">SignIn</button>
+					</div>
+				</div>
+			</div>
+	        <form class = "search">				
+				<input type = "text" placeholder = "Pesquisar...">
+				<!--<button type = "submit">Pesquisar</button>-->
+			</form>
+			<!-- Botão para mudar o tema do site-->
+			<div class = "ThemeMenu-Skin">
+				<button id = "ThemeMenu-ButtonJS" class = "BtThemeMenu"> 
+					<img style = "width: 20px; height: 20px;" src = "imagens/tema.png">
+				</button>
+				<div id = "BtThemeMShow" class = "ThemeMenu-Items">	
+					<div class = "CloseMenu">
+						<button id = "BtTheme-CloseJS" class = "BtCloseMenu"> 
+							<img style = "width: 20px; height: 20px;" src = "imagens/close.png">
+						</button>
+					</div>
+					<div class = "ThemeMenu-Options">
+						<button class = "LightTheme">Light Theme</button>
+						<button class = "DarkTheme">Dark Theme</button>
+						<button class = "BlueTheme">Blue Theme</button>
+						<button class = "GreenTheme">Green Theme</button>
+						<button class = "PurpleTheme">Purple Theme</button>
+					</div>
+				</div>	
+			</div>
+			<!-- Menu do utilizador-->
+			<div class = "UserMenu-Skin">
+				<button id = "DPmenuUser-ButtonJS" class = "BtUserMenu"> 
+					<img style = "width: 24px; height: 24px;" src = "imagens/add_user.png">
+				</button>
+
+				<div id = "BtUserMShow" class = "UserMenu-Items">	
+					<nav class = "UserMenu-List">
+						<a href = "####">opcao1</a>
+						<a href = "####">opcao2</a>
+					</nav>
+
+					<div class = "UserMenu-Options">
+						<button id = "BtLogInJS" class = "BtLogIn">LogIn</button>
+						<button id = "BtSignInJS" class = "BtSignIn">SignIn</button>
+					</div>
+				</div>	
+			</div>
 		</header>
 
-		<nav class = "menu">
-			<ul>
-				<p><a href = "####">opcao1</a></p>
-				<p><a href = "####">opcao2</a></p>
-			</ul>
-		</nav>
+		<div class = "ContentSite">
+			<nav class = "menu-lateral">
+				<ul class = "MenuList">
+					<p><a href = "####">opcao1</a></p>
+					<p><a href = "####">opcao2</a></p>
+				</ul>
+			</nav>
+			<article class = "MainArticle">
+				<article class = "ArticleContent">
+					<div class = "ComponentList">
 
-		<article>
-			<p> bjabababababab</p>
-		</article>
+					</div>
+				</article>
 
-		<article>
-			<p> bjabababababab</p>
-		</article>
+				<div class = "BD-Options">
 
+					<div class = "BtAddStyle">
+						<button id = "BtAddComponentJS" type = "Submit" class = "BtAddComponent">
+							<img style = "width: 85px; height: 85px;" src = "imagens/add_componente.png">							</button>
+					</div>
+
+					<div class = "BtChangeStyle">
+						<button id = "BtChangeComponentJS" class = "BtChangeComponent">
+							<img style = "width: 85px; height: 85px;" src = "imagens/change_componente.png">							
+						</button>
+					</div>
+
+					<div class = "BtDeleteStyle">
+						<button id = "BtDeleteComponentJS" type = "Delete" class = "BtDeleteComponent">
+							<img style = "width: 85px; height: 85px;" src = "imagens/delete_componente.png">							
+						</button>
+					</div>
+				</div>
+			</article>
+		</div>
 		<footer>
 			<p>Projeto PAP</p>
 		</footer>
 	</main>
-<html>
+	<script src = "ScriptsProjeto.js"></script>
+</html>
+<?php
+	$con = new Mysqli("localhost", "root", "", "pc_builder");
+	if($con->connect_error != 0){
+		echo "Ocorreu um erro de ligação à base de dados; " . $con->connect_error;
+		exit;
+	}
+	$stn = $con->prepare("INSERT INTO componentes VALUES (0,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+	$stn -> bind_param("sssssssssssss",
+		$_POST["nome"], 
+		$_POST["tipo"], 
+		$_POST["marca"], 
+		$_POST["modelo"], 
+		$_POST["preco"],
+		$_POST["loja"], 
+		$_POST["stock"],
+		$_POST["imagem"], 
+		$_POST["descricao"], 
+		$_POST["socket_cpu"], 
+		$_POST["tipo_ram"], 
+		$_POST["slots_ram"], 
+		$_POST["tipo_pcie_principal"]
+	);
+
+	$stn ->execute();
+	$stn ->close();
+	$con ->close();
+?>
