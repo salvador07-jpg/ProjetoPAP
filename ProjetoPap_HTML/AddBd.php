1cc9597ae0d79907acb17402c2633be1dfb06bdc
salvador07-jpg <salvacruz200708@gmail.com>
2026-03-15 18:58:26 +0000

    Base do menu inicial

--- ProjetoPap_HTML/AddBd.php
+++ ProjetoPap_HTML/AddBd.php
@@ -1,37 +1,3 @@
-<?php
-	if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["nome"])){
-
-		$con = new Mysqli("localhost", "root", "", "pc_builder");
-
-		if ($con->connect_error) {
-			die("Erro na ligação à base de dados: " . $con->connect_error);
-		}
-
-		$stn = $con->prepare("INSERT INTO componentes 
-			(id, nome, tipo, marca, modelo, preco, loja, stock, imagem, descricao, socket_cpu, tipo_ram, slots_ram, tipo_pcie_principal) 
-			VALUES (0,?,?,?,?,?,?,?,?,?,?,?,?,?)");
-
-		$stn->bind_param("sssssssssssss",
-			$_POST["nome"],
-			$_POST["tipo"],
-			$_POST["marca"],
-			$_POST["modelo"],
-			$_POST["preco"],
-			$_POST["loja"],
-			$_POST["stock"],
-			$_POST["imagem"],
-			$_POST["descricao"],
-			$_POST["socket_cpu"],
-			$_POST["tipo_ram"],
-			$_POST["slots_ram"],
-			$_POST["tipo_pcie_principal"]
-		);
-
-		$stn->execute();
-		$stn->close();
-		$con->close();
-	}
-?>
 <!DOCTYPE html>
 <html lang = "pt">
 	<head>
@@ -44,8 +10,6 @@
 			<!-- Botões -->
 			<link rel = "stylesheet" href = "FeBtAparenciaBotoes.css">
 			<link rel = "stylesheet" href = "FeBtMenu.css">
-			<link rel = "stylesheet" href = "FeBtUtilizador.css">
-			<link rel = "stylesheet" href = "FeBtTemaSite.css">
 			<link rel = "stylesheet" href = "FeBtAddBD.css">
 
 			<!-- Ficheiro responsivo é por último para conseguir se sobrepor aos estilos base -->
@@ -76,44 +40,7 @@
 				<input type = "text" placeholder = "Pesquisar...">
 				<!--<button type = "submit">Pesquisar</button>-->
 			</form>
-			<!-- Botão para mudar o tema do site-->
-			<div class = "ThemeMenu-Skin">
-				<button id = "ThemeMenu-ButtonJS" class = "BtThemeMenu"> 
-					<img style = "width: 20px; height: 20px;" src = "imagens/tema.png">
-				</button>
-				<div id = "BtThemeMShow" class = "ThemeMenu-Items">	
-					<div class = "CloseMenu">
-						<button id = "BtTheme-CloseJS" class = "BtCloseMenu"> 
-							<img style = "width: 20px; height: 20px;" src = "imagens/close.png">
-						</button>
-					</div>
-					<div class = "ThemeMenu-Options">
-						<button class = "LightTheme">Light Theme</button>
-						<button class = "DarkTheme">Dark Theme</button>
-						<button class = "BlueTheme">Blue Theme</button>
-						<button class = "GreenTheme">Green Theme</button>
-						<button class = "PurpleTheme">Purple Theme</button>
-					</div>
-				</div>	
-			</div>
-			<!-- Menu do utilizador-->
-			<div class = "UserMenu-Skin">
-				<button id = "DPmenuUser-ButtonJS" class = "BtUserMenu"> 
-					<img style = "width: 24px; height: 24px;" src = "imagens/add_user.png">
-				</button>
 
-				<div id = "BtUserMShow" class = "UserMenu-Items">	
-					<nav class = "UserMenu-List">
-						<a href = "####">opcao1</a>
-						<a href = "####">opcao2</a>
-					</nav>
-
-					<div class = "UserMenu-Options">
-						<button id = "BtLogInJS" class = "BtLogIn">LogIn</button>
-						<button id = "BtSignInJS" class = "BtSignIn">SignIn</button>
-					</div>
-				</div>	
-			</div>
 		</header>
 
 		<div class = "ContentSite">
